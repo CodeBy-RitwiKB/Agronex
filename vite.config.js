75f6c5adc3d9d2fac46b057e269813babcef8fd7
Ritwik Banerjee <kritwikbanerjee27@gmail.com>
2026-05-11 15:50:43 +0530

    update package and vite files

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,20 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('lucide-react')) return 'icons';
+            if (id.includes('framer-motion')) return 'animations';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@bloom': path.resolve(__dirname, './src/lib/bloom')
